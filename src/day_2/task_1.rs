0e1465529195503a0ca754e9cef81f81d0512f92
Evaldas Tyla <evaldas.tyla@gmail.com>
2019-12-09 14:21:46 +0200

    Day 2 Task 1 implementation

--- src/day_2/task_1.rs
+++ src/day_2/task_1.rs
@@ -1,19 +1,53 @@
 use std::fs::File;
 use std::io::{BufReader, Lines};
 
-use advent_of_code_2019::utils;
-
 pub fn calculate(lines: Lines<BufReader<File>>) -> String {
-    let input: Vec<u32> = lines
+    let mut input: Vec<u32> = lines
         .collect::<Result<String, _>>()
         .unwrap()
         .split(',')
         .map(|item| item.parse::<u32>().expect("Failed parsing int value"))
         .collect();
 
-    let opcode_chunks: Vec<&[u32]> = input.chunks(4).collect();
+    initiate_gravity_restore_assist(&mut input);    
+    let result = process_input(&mut input);    
 
-    println!("{:?}", opcode_chunks);
+    result.to_string()
+}
 
-    "unfinished".to_owned()
+fn process_input(input: &mut [u32]) -> u32 {    
+    for index in (0..input.len()).step_by(4) {        
+        let operation: u32 = input[index];
+        let op_indices = get_opcode_indices(index, input);
+
+        match operation {
+            1 => add(op_indices, input),
+            2 => multiply(op_indices, input),
+            99 => return input[0],                
+            _ => panic!("Undefined opcode")
+        }
+    }
+
+    0
+}
+
+fn add(indices: (usize, usize, usize), input: &mut [u32]) {
+    input[indices.0] = input[indices.1] + input[indices.2]
+}
+
+fn multiply(indices: (usize, usize, usize), input: &mut [u32]) {    
+    input[indices.0] = input[indices.1] * input[indices.2]
+}
+
+fn initiate_gravity_restore_assist(input: &mut [u32]) {
+    input[1] = 12;
+    input[2] = 2;
+}
+
+fn get_opcode_indices(index: usize, input: &[u32]) -> (usize, usize, usize) {
+    let result_index = input[index + 3] as usize;
+    let number_one_index = input[index + 1] as usize;
+    let number_two_index = input[index + 2] as usize;
+
+    (result_index, number_one_index, number_two_index)
 }
